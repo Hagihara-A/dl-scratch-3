ec9607389bea81a77bcdc87ada2118df3e8fd5e2
Arark <knkr5456@gmail.com>
2021-05-25 02:22:18 +0900

    test: add test__dot_var

---
+++ test/test_utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+from dezero.Core import Variable, square
+from dezero.utils import get_dot_graph
+from unittest import TestCase
+
+
+class TestGetDotGraph(TestCase):
+    def test__dot_var(self):
+        a = Variable(np.array(1.0))
+        b = Variable(np.array(1.0))
+        c = a + b
+        d = square(c)
+
+        actual = get_dot_graph(d)
+        self.assertIn(f"{id(d.creator)} -> {id(d)}", actual)
+        self.assertIn(f"{id(c)} -> {id(d.creator)}", actual)
