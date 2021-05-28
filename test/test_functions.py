6cf2b9e4442cc27c0fddb98b86b6442ab6ca11c6
Hagihara-A <knkr5456@gmail.com>
2021-05-28 13:51:29 +0900

    test: Reshape

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 from dezero.core import Variable
-from dezero.functions import tanh
+from dezero.functions import reshape, tanh
 from unittest import TestCase
 
 
@@ -15,3 +15,18 @@ class TanhTest(TestCase):
         y = tanh(x)
         y.backward()
         self.assertAlmostEqual(x.grad.data, np.array(0.7864477330213905))
+
+
+class ReshapeTest(TestCase):
+    def test_forward(self):
+        x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+        y = reshape(x, (6,))
+        self.assertEqual(y.shape, (6,))
+
+    def test_backward(self):
+        x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+        y = reshape(x, (6,))
+        y.backward(retain_grad=True)
+        self.assertEqual(y.grad.shape, (6,))
+        self.assertEqual(x.grad.shape, x.shape)
+        self.assertTrue((x.grad.data == np.ones((2, 3))).all())
