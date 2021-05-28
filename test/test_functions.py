737d3dbec0fb29fe20086bac9753abd4a8f2c31f
Hagihara-A <knkr5456@gmail.com>
2021-05-28 14:03:41 +0900

    test: Transpose

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 from dezero.core import Variable
-from dezero.functions import reshape, tanh
+from dezero.functions import reshape, tanh, transpose
 from unittest import TestCase
 
 
@@ -30,3 +30,16 @@ class ReshapeTest(TestCase):
         self.assertEqual(y.grad.shape, (6,))
         self.assertEqual(x.grad.shape, x.shape)
         self.assertTrue((x.grad.data == np.ones((2, 3))).all())
+
+
+class TransposeTest(TestCase):
+    def test_forward(self):
+        x = Variable(np.arange(6).reshape(2, 3))
+        y = transpose(x)
+        self.assertEqual(y.shape, (3, 2))
+
+    def test_backward(self):
+        x = Variable(np.arange(6).reshape(2, 3))
+        y = transpose(x)
+        y.backward()
+        self.assertEqual(x.grad.shape, x.shape)
