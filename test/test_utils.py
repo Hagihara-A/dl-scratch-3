c34e192113274f1faa418aa8fd64e857b244f7fb
Arark <knkr5456@gmail.com>
2021-06-04 11:54:00 +0900

    test(to_onehot): add test_1d

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 import numpy as np
 from dezero.core import Variable
 from dezero.functions import square
-from dezero.utils import _dot_var, get_dot_graph
+from dezero.utils import _dot_var, get_dot_graph, to_onehot
+from numpy.testing import assert_equal
 
 
 class TestGetDotGraph(TestCase):
@@ -29,3 +30,11 @@ class Test_dot_var(TestCase):
         txt = _dot_var(a, verbose=True)
         self.assertEqual(
             f'{id(a)} [label="{a.shape} {a.dtype}", color=orange, style=filled]\n', txt)
+
+
+class ToOnehotTest(TestCase):
+    def test_1d(self):
+        t = np.array([2, 1, 3, 0])
+        t_onehot = to_onehot(t, labels=4)
+        expected = [[0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0]]
+        assert_equal(t_onehot, expected)
