d56750b7e3e4e316ab69bb5df2d0d5bc8fa7dbd3
Arark <knkr5456@gmail.com>
2021-06-10 10:34:28 +0900

    feat(Layer): impl Conv2d

--- dezero/layers.py
+++ dezero/layers.py
@@ -5,6 +5,7 @@ import weakref
 from dezero.core import Parameter, Variable
 import numpy as np
 import dezero.functions as F
+from dezero.utils import pair
 import os
 
 
@@ -99,3 +100,41 @@ class Linear(Layer):
             self._init_W()
         y = F.linear(x, self.W, self.b)
         return y,
+
+
+class Conv2d(Layer):
+    def __init__(self, out_channels: int, kernel_size: int, stride=1, pad=0,
+                 nobias=False, dtype=np.float32, in_channels=None):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.dtype = dtype
+
+        self.W = Parameter(None, name="W")
+        if in_channels is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_channels, dtype=dtype), name="b")
+
+    def _init_W(self):
+        C, OC = self.in_channels, self.out_channels
+        KH, KW = pair(self.kernel_size)
+        scale = np.sqrt(1/(C*KH*KW))
+        W_data = np.random.randn(OC, C, KH, KW).astype(self.dtype) * scale
+        self.W_data = W_data
+
+    def forward(self, *xs: np.ndarray) -> tuple[Variable, ...]:
+        x, = xs
+        if self.W_data is None:
+            self.in_channels = x.shape[1]
+            self._init_W()
+
+        y = F.conv2d_simple(x, self.W, self.b, self.stride, self.pad)
+        return y,
+
